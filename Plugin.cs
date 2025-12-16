d325caf754cc5e879bebb4135a711f0584724330
Mas <3masstro3@gmail.com>
2025-12-16 06:40:41 +0300

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using System;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Lozaros_FPSBooster.Patches
+{
+    [Description(Lozaros_FPSBooster.PluginInfo.Description)]
+    [BepInPlugin(Lozaros_FPSBooster.PluginInfo.GUID, Lozaros_FPSBooster.PluginInfo.Name, Lozaros_FPSBooster.PluginInfo.Version)]
+    public class HarmonyPatches : BaseUnityPlugin
+    {
+        private static bool fpsBoostApplied = false;
+        private static float lastCacheClearedTime = 0f;
+
+        private void Awake()
+        {
+            FPSboost();
+        }
+
+        private void Update()
+        {
+            if (!fpsBoostApplied)
+            {
+                FPSboost();
+            }
+
+            AutoClearCache();
+        }
+
+        public static void FPSboost()
+        {
+            QualitySettings.globalTextureMipmapLimit = 999999999;
+            QualitySettings.maxQueuedFrames = 60;
+            Application.targetFrameRate = 144;
+            fpsBoostApplied = true;
+        }
+
+        public static void AutoClearCache()
+        {
+            if (Time.time > lastCacheClearedTime)
+            {
+                lastCacheClearedTime = Time.time + 60f;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                Resources.UnloadUnusedAssets();
+            }
+        }
+    }
+}
---
+++ RigPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Lozaros_FPSBooster.Patches
+{
+    [HarmonyPatch(typeof(VRRig), "OnDisable")]
+    internal class GhostPatch : MonoBehaviour
+    {
+        public static bool Prefix(VRRig __instance)
+        {
+            return !(__instance == GorillaTagger.Instance.offlineVRRig);
+        }
+    }
+
+    [HarmonyPatch(typeof(VRRigJobManager), "DeregisterVRRig")]
+    public static class GhostPatch2
+    {
+        public static bool Prefix(VRRigJobManager __instance, VRRig rig)
+        {
+            return !(__instance == GorillaTagger.Instance.offlineVRRig);
+        }
+    }
+}
